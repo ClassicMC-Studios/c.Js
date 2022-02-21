48cf8f4cf7558d54976cebae8adfed13212e67ee
ClassicMC <classicmclive@gmail.com>
2022-02-20 23:56:36 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -146,27 +146,4 @@ class Cjs{
     }
 }
 const cjs = new Cjs();
-let pos = 5;
-cjs.rect(pos,"#00cc00");
-document.addEventListener('keydown', function (evt){
-    if(event.keyCode == 40){
-        pos += 8;
-        cjs.clear();
-        cjs.rect(pos,"#00cc00");
-    }
-    if(event.keyCode == 38){
-        pos -= 8;
-        cjs.clear();
-        cjs.rect(pos,"#00cc00");
-    }
-    if(event.keyCode == 37){
-        pos -= 1;
-        cjs.clear();
-        cjs.rect(pos,"#00cc00");
-    }
-    if(event.keyCode == 39){
-        pos += 1;
-        cjs.clear();
-        cjs.rect(pos,"#00cc00");
-    }
-});
+
