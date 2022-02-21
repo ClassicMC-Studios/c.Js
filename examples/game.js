6d2fcb5d62ed38e09d25b880dc68b17f62fd32e0
ClassicMC <classicmclive@gmail.com>
2022-02-21 10:35:46 -0500

    Add files via upload

---
+++ examples/game.js
@@ -0,0 +1,33 @@
+const cjs = new Cjs();
+let pos = 5;
+cjs.rect(9,"#a6a6a6");
+cjs.rect(pos,"#00cc00");
+document.addEventListener('keydown', function (evt){
+    if(event.keyCode == 40){
+        pos += 8;
+        cjs.clear();
+        cjs.rect(9,"#a6a6a6");
+        cjs.rect(pos,"#00cc00");
+    }
+    if(event.keyCode == 38){
+        pos -= 8;
+        cjs.clear();
+        cjs.rect(9,"#a6a6a6");
+        cjs.rect(pos,"#00cc00");
+    }
+    if(event.keyCode == 37){
+        pos -= 1;
+        cjs.clear();
+        cjs.rect(9,"#a6a6a6");
+        cjs.rect(pos,"#00cc00");
+    }
+    if(event.keyCode == 39){
+        pos += 1;
+        cjs.clear();
+        cjs.rect(9,"#a6a6a6");
+        cjs.rect(pos,"#00cc00");
+    }
+    if(pos == 9){
+        alert("You've got mail!");
+    }
+});
