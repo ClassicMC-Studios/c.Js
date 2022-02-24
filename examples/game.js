45b1da669b2b5e8b1ee059a6b24363274ecd1f8c
ClassicMC <classicmclive@gmail.com>
2022-02-24 10:29:52 -0500

    Add files via upload

--- examples/game.js
+++ examples/game.js
@@ -1,41 +1,42 @@
 const cjs = new Cjs();
 let pos = 5;
-cjs.rect(9,"#a6a6a6");
-cjs.rect(47,"#ffff00");
-cjs.rect(pos,"#00cc00");
+let po = 47;
+let score = 0;
+function redraw(){
+    cjs.clear();
+    cjs.rect(po,"#ffff00");
+    cjs.rect(pos,"#00cc00");
+    cjs.text("Score:"+score,100,90);
+}
+redraw();
+window.main = function (){
+    window.requestAnimationFrame( main );
+    if ( pos == po){
+        po = cjs.getRandomInt(48);
+        if(po == 0){
+            po ++;
+        }
+        score ++;
+        redraw();
+    }
+}
 document.addEventListener('keydown', function (evt){
     if(event.keyCode == 40){
         pos += 8;
-        cjs.clear();
-        cjs.rect(47,"#ffff00");
-        cjs.rect(9,"#a6a6a6");
-        cjs.rect(pos,"#00cc00");
+        redraw();
     }
     if(event.keyCode == 38){
         pos -= 8;
         cjs.clear();
-        cjs.rect(47,"#ffff00");
-        cjs.rect(9,"#a6a6a6");
-        cjs.rect(pos,"#00cc00");
+        redraw();
     }
     if(event.keyCode == 37){
         pos -= 1;
-        cjs.clear();
-        cjs.rect(47,"#ffff00");
-        cjs.rect(9,"#a6a6a6");
-        cjs.rect(pos,"#00cc00");
+        redraw();
     }
     if(event.keyCode == 39){
         pos += 1;
-        cjs.clear();
-        cjs.rect(47,"#ffff00");
-        cjs.rect(9,"#a6a6a6");
-        cjs.rect(pos,"#00cc00");
-    }
-    if(pos == 9){
-        alert("You've got mail!");
-    }
-    else if ( pos == 47){
-        alert("You've got more mail!");
+        redraw();
     }
 });
+main();
